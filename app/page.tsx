68cdb3ebe2dc110a956fa0f733ffd0501b741202
wynter <wynters@email.sc.edu>
2025-09-29 21:22:10 -0400

    adding react query

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
-import Greeting from "@/components/Greeting";
+"use client"
+
+import { useQuery } from "@tanstack/react-query";
+
+type Post = {
+    userID: number,
+    id: number,
+    title: string,
+    body: string,
+}
+
+async function fetchPosts(): Promise<Post[]> {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    //only use await in async functions
+    return response.json()
+}
 
 export default function Home() {
+    const { data, error, isLoading, isError } = useQuery({ queryKey: ["posts"], queryFn: fetchPosts });
+
+    if(isLoading) {
+        return <p>Loading... </p>
+    }
+
+    if (isError) {
+        return <p className="text-red-500">Error: {(error as Error).message}</p>
+    }
+
     return (
-        <section className="bg-teal-100 min-h-screen flex flex-col items-center">
-            <h1 className="text-3xl font-bold text-blue-600 text-center pt-4">Hello World</h1>
-            <button className="mt-4 rounded bg-black px-4 py-2 text-white cursor-pointer hover:bg-gray-700">Click me
-            </button>
-            <Greeting name="KTP" />
-            <Greeting name="World" />
-            <Greeting name="Braden" />
-        </section>
+        <div>
+            {data?.map(function (post, key) {
+                return <div key={key} className="border border-solid border-white p-4">
+                    <p> Title: {post.title}</p>
+                    <p> Body: {post.body}</p>
+                </div>;
+            })}
+        </div>
     );
 }
---
+++ public/store.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand/react";
+import { persist } from "zustand/middleware";
+
+type CounterStore = {
+    count: number
+
+    increment: () => void;
+    decrement: () => void;
+}
+
+export const useCounterStore = create<CounterStore>(
+    (set) => ({
+        count: 0,
+        increment: () => {
+            set((state) => ({ count: state.count + 1 }))
+        },
+        decrement: () => {
+            set((state) => ({ count: state.count - 1 }))
+        }
+    })
+);
+
+export const usePersistCounterStore = create<CounterStore>()(
+    persist ((set) => ({
+        count: 0,
+        increment: () => {
+            set((state) => ({ count: state.count + 1 }))
+        },
+        decrement: () => {
+            set((state) => ({ count: state.count - 1 }))
+        }
+    }),
+        {
+            name: "counterStore"
+        }
+    )
+);
